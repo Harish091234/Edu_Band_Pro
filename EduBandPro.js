d247029da58c4ed32f4048cdd7526567d6962d5b
Harish091234 <115438@student.bmg.vic.edu.au>
2024-06-04 12:41:48 +1000

    Add files via upload

---
+++ EduBandPro.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("stats-button");
+var stats = document.getElementById("stats");
+
+button.addEventListener("click", function() {
+	stats.style.display = "block";
+});
